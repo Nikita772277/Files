af2073dfb63649ebf9960c41aec16a005e52bda5
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-11-11 12:20:38 +0300

    Добавьте файлы проекта.

---
+++ Files/Program.cs
@@ -0,0 +1,49 @@
+﻿using Files;
+
+WorkWithFile workwithfile=new WorkWithFile();
+void GetMenu()
+{
+    Console.WriteLine();
+    Console.WriteLine($"1) Создание файла");
+    Console.WriteLine($"2) перемещение файла");
+    Console.WriteLine($"3) создание множества файлов");
+    Console.WriteLine($"4) Удаление");
+    Console.WriteLine($"5) Добовление текста");
+    Console.WriteLine($"6) Удалить несколько файлов");
+    Console.WriteLine();
+}
+void Menu()
+{
+    while (true)
+    {
+        GetMenu();
+        string enter = Console.ReadLine();
+        bool chek = int.TryParse(enter, out int result);
+        if (result == 1)
+        {
+            workwithfile.CreatFile();
+        }
+        else if (result == 2)
+        {
+            workwithfile.FileMoving();
+        }
+        else if (result == 3)
+        {
+            workwithfile.MultipleFilesCreat();
+        }
+        else if (result == 4)
+        {
+            workwithfile.DeleteFile();
+        }
+        else if (result == 5)
+        {
+            workwithfile.Wrate();
+        }
+        else if (result == 6)
+        {
+            workwithfile.MultipleFilesDelete();
+        }
+        else { Console.WriteLine($"Выберите пункт из меню"); }
+    }
+}
+Menu();
---
+++ Files/WorkWithFile.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    internal class WorkWithFile
+    {
+        //private string file = @"C:\Users\Студент 3\Desktop\1\file.txt";
+        //private string file2 = @"C:\Users\Студент 3\Desktop\2\index.txt";
+        public void CreatFile() 
+        {
+            Console.WriteLine($"В какую папку добавить файл");
+            string name=Console.ReadLine();
+            Console.WriteLine($"Введите название файла");
+            string enter=Console.ReadLine();
+            string file = $@"C:\Users\Студент 3\Desktop\{name}\{enter}.txt";
+            FileInfo fileInf = new FileInfo(file);
+            if (!fileInf.Exists)
+            {
+                fileInf.Create();
+                Console.WriteLine($"Файл создан");
+            }
+            else { Console.WriteLine($"Такой файл уже существует"); }
+
+        }
+        public void FileMoving()
+        {
+            Console.WriteLine($"Из какой папки переместить файл");
+            string wherefrom = Console.ReadLine();
+            Console.WriteLine($"В какую папку переместить файла");
+            string destination = Console.ReadLine();
+            string file = $@"C:\Users\Студент 3\Desktop\{wherefrom}\file.txt";
+            string file2 = $@"C:\Users\Студент 3\Desktop\{destination}\index.txt";
+            FileInfo fileinf =new FileInfo(file);
+            if (fileinf.Exists)
+            {
+                fileinf.MoveTo(file2);
+                Console.WriteLine($"Файл перемещён");
+            }
+            else
+            {
+                Console.WriteLine($"Такой файл уже существует в папке назначения");
+            }
+        }
+        public void MultipleFilesCreat()
+        {  
+            for (int i = 0; i<1000;i++)
+            {
+                string file3 = $@"C:\Users\Студент 3\Desktop\2\file{i}.txt";
+                FileInfo fileInf = new FileInfo(file3);
+                fileInf.Create();
+            }
+            Console.WriteLine($"Создана 1000 файлов");
+        }
+        public void MultipleFilesDelete()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                string file3 = $@"C:\Users\Студент 3\Desktop\2\file{i}.txt";
+                FileInfo fileInf = new FileInfo(file3);
+                fileInf.Delete();
+            }
+            Console.WriteLine($"удалено 1000 файлов");
+        }
+        public void DeleteFile()
+        {
+            Console.WriteLine($"Название паки из которой вы хотите удалить файл");
+            string folder=Console.ReadLine();
+            Console.WriteLine($"Введите название файла который вы хотите удалить");
+            string enter=Console.ReadLine();
+            string file = $@"C:\Users\Студент 3\Desktop\{folder}\{enter}.txt";
+            FileInfo fileInf = new FileInfo(file);
+            if (fileInf.Exists)
+            {
+                fileInf.Delete();
+                Console.WriteLine($"Файл удалён");
+            }
+            else { Console.WriteLine($"Нет файла с таким названием"); }
+        }
+        public async void Wrate()
+        {
+            Console.WriteLine($"В какой папке файл");
+            string folder = Console.ReadLine();
+            Console.WriteLine($"В какой файл записать данные");
+            string enter = Console.ReadLine();
+            Console.WriteLine($"Что добавить в файл");
+            string text = Console.ReadLine(); 
+            string file = $@"C:\Users\Студент 3\Desktop\{folder}\{enter}.txt";
+            using (FileStream fileStream=new FileStream(file, FileMode.Open))
+            {
+                byte[] buffer = Encoding.Default.GetBytes(text);
+                await fileStream.WriteAsync(buffer, 0, buffer.Length);
+                Console.WriteLine("текст записан в файл");
+            }
+            using (FileStream fileStream = File.OpenRead(file)) ;
+        }
+    }
+}
